7a03ce8042b280c3382f3bf1370f345ef4700d0c
anhdoo <anhdoo@gmail.com>
2025-07-05 00:17:21 +0700

    echo command

--- src/main.rs
+++ src/main.rs
@@ -44,25 +44,32 @@ async fn handler(mut stream: TcpStream, addr: SocketAddr) {
 
                 for line in received.split("\n") {
                     let line = line.trim();
-                    if line == "PING" {
-                        let response = "+PONG\r\n";
-                        write_stream(&mut stream, addr, response).await;
-                    } 
-
-                    if line.len() >= 4 && &line[0..4] == "ECHO" {
-                        if line.len() == 4 {} //just only "echo"
-                        else if &line[..=4] != "ECHO " {} //Invalid command ECHO(sahdisabud)
-                        else{
-                            //len > 4
-                            let mut pharase = &line[4..line.len()];
-                            let pharase = pharase.trim();
-                            println!("{}", pharase);
-                            // let response = format!("$3\r\n{}\r\n", pharase);
-                            let response = format!("${}\r\n{}\r\n", pharase.len(), pharase);
-                            
-                            write_stream(&mut stream, addr, &response.as_str()).await;
+                    match line.split_whitespace().next() {
+                        Some("PING") => {
+                            let response = "+PONG\r\n";
+                            write_stream(&mut stream, addr, response).await;
                         }
+                        Some("ECHO") => {
+                            if let Some(payload ) = line.split_whitespace().nth(1){
+                                let payload = format!("${}\r\n{}\r\n", payload.len(), payload);
+                                write_stream(&mut stream, addr, payload.as_str()).await;
+                            }
+                        }
+                        _ => println!(),
                     }
+
+                    // if line.len() >= 4 && &line[0..4] == "ECHO" {
+                    //     if line.len() == 4 {} //just only "echo"
+                    //     else if &line[..=4] != "ECHO " {} //Invalid command ECHO(sahdisabud)
+                    //     else{
+                    //         //len > 4
+                    //         let pharase = &line[4..line.len()].trim();
+                    //         println!("{}", pharase);
+                    //         let response = format!("${}\r\n{}\r\n", pharase.len(), pharase);
+
+                    //         write_stream(&mut stream, addr, &response.as_str()).await;
+                    //     }
+                    // }
                 }
             }
             Ok(_) => {
@@ -81,7 +88,7 @@ async fn write_stream(stream: &mut TcpStream, addr: SocketAddr, payload: &str) {
     if stream.write_all(payload.as_bytes()).await.is_ok() {
         stream.flush().await.expect("Failed to flush stream");
         println!("Sent to {:?}: {}", addr, payload.trim());
+    } else {
+        println!("Failed to send response to {:?}", addr);
     }
-
-    println!("Failed to send response to {:?}", addr);
 }
