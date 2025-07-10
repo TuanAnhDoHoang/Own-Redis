1dc0b01d7f9d8865ef51343dfbd46df02dcc3767
anhdoo <anhdoo@gmail.com>
2025-07-10 07:19:59 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,9 @@ async fn main() {
     println!("Logs from your program will appear here!");
     let args: Vec<String> = env::args().collect();
 
-    let (redis_database, rdb_file) = flags_handler(args.into_iter().skip(1).collect()).unwrap();
+    let (redis_database, rdb_file, port) = flags_handler(args.into_iter().skip(1).collect()).unwrap();
 
-    let listener = TcpListener::bind("127.0.0.1:6379")
+    let listener = TcpListener::bind(format!("127.0.0.1:{}",port))
         .await
         .unwrap_or_else(|e| {
             eprintln!("Failed to bind to address: {}", e);
@@ -77,8 +77,10 @@ async fn stream_handler(stream: TcpStream, mut redis_database: RedisDatabase, mu
     }
 }
 
-fn flags_handler(flags: Vec<String>) -> Result<(RedisDatabase, RdbFile)> {
+fn flags_handler(flags: Vec<String>) -> Result<(RedisDatabase, RdbFile, usize)> {
     let mut redis_database = RedisDatabase::new();
+    let mut rdb_file = RdbFile::new();
+    let mut port: usize = 6379;
     let mut index = 0;
     while index < flags.len() {
         let flag_name = flags[index].clone();
@@ -95,6 +97,12 @@ fn flags_handler(flags: Vec<String>) -> Result<(RedisDatabase, RdbFile)> {
                 }
                 None => panic!("Need a file name"),
             },
+            "--port" => match flags.get(index + 1) {
+                Some(port_number) => {
+                    port = port_number.parse::<usize>().expect("Error when parse port number");
+                }
+                None => panic!("Need a file name"),
+            },
             _ => panic!("Invalid flags name: {}", flag_name),
         }
         index += 2;
@@ -103,12 +111,11 @@ fn flags_handler(flags: Vec<String>) -> Result<(RedisDatabase, RdbFile)> {
     let path: PathBuf = PathBuf::from(redis_database.get_dir().unwrap_or("./".into()))
     .join(redis_database.get_dir_file_name().unwrap_or("dump.rdb".into()));
 
-    let mut rdb_file = RdbFile::new();
     if path.exists(){
         (_, rdb_file) = parse_rdb::parse_rdb_file(
             std::fs::read(path).unwrap().as_slice()
         ).unwrap();
         // println!("LOG_FROM_flags_hanlder --- rdb_file: {:?}", rdb_file);
     }
-    Ok((redis_database, rdb_file))
+    Ok((redis_database, rdb_file, port))
 }
